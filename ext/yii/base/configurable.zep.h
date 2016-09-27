7f3dc9da0c9fa9053e40c00bff215c44e533ab04
fightingbamboo <fightingbamboo@gmail.com>
2016-09-27 23:48:03 +0800

    delete gen c code

--- ext/yii/base/configurable.zep.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *yii_base_configurable_ce;
-
-ZEPHIR_INIT_CLASS(yii_base_Configurable);
-
--- ext/yii/base/viewevent.zep.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *yii_base_viewevent_ce;
-
-ZEPHIR_INIT_CLASS(yii_base_ViewEvent);
-
